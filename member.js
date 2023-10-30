96f909fd7135273e5aebd577859da859da80b1e6
afonso10o <90221555+afonso10o@users.noreply.github.com>
2023-10-30 15:32:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var member = document.getElementById("skills-member");
+  var memberOptions = document.getElementById("skills-member-options");
+  memberOptions.classList.toggle("show");
+  member.classList.toggle("active");
+}
